02c2dcc4eb3d995d3622f8ab1ee9b6474bae073b
DashPgi <pajiparsa@gmail.com>
2026-08-31 04:45:03 +0330

    update30

--- phase-char/SQLQuery1.sql
+++ phase-char/SQLQuery1.sql
@@ -1,2 +1,6 @@
-select *
-From [Production].[ProductCategory]
+Select custid ,orderid,val,
+LOG(val) as pval,
+LEAD(val) as nval
+First_Value(val) as fval,   
+Last_Value(val) OVER(Partition by custid order by orderid rows between current row and 2 following) as last_3val
+Last_Value(val) OVER(Partition by custid order by orderid rows between unbounded preceding and current row) as last_val
