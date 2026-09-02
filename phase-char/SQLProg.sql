07f7bc17822064b7529e558a608b61093c7140a4
DashPgi <pajiparsa@gmail.com>
2026-09-02 08:16:02 +0330

    update 39

--- phase-char/SQLProg.sql
+++ phase-char/SQLProg.sql
@@ -14,3 +14,16 @@ go
 print 'hello world';
 select @s
 go
+
+-- conditional statements
+
+if YEAR(GETDATE()) = 2026
+BEGIN
+    print 'It is 2026';
+    PRINT 'HAPPY NEW YEAR';
+END
+else
+
+    print 'It is not 2026';
+
+print 'It is not 2026';
