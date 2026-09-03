1d2f3879b11f83abdb7b3e0decd8892f949f10b6
DashPgi <pajiparsa@gmail.com>
2026-09-04 01:45:26 +0330

    Add locks and isolation level details to documentation; update SQL transaction example with isolation level settings

--- phase-char/SQLTrans.sql
+++ phase-char/SQLTrans.sql
@@ -11,3 +11,11 @@ SET Balance = Balance + 100
 WHERE Id = 2;
 
 COMMIT TRANS;
+
+-- Isolation Level 
+-- READ UNCOMMITTED
+
+SET ISOLATION LEVEL READ UNCOMMITTED;
+SELECT * FROM account; -- Dirty Page    
+FROM Porduction.Profuct with(nolock); -- Lock free read
+
