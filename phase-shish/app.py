d44bffcbeddf8f60efe42172a1664fd63b6131b2
DashPgi <pajiparsa@gmail.com>
2026-09-17 15:34:57 +0330

    Add .gitignore for virtual environment and refactor app.py to import Flask. Move Student class implementation to doc.py for better organization.

--- phase-shish/app.py
+++ phase-shish/app.py
@@ -1,15 +1,1 @@
-# Mutability
-class Student:
-    def __init__(self, name: str, grades: list[int] = None):
-        self.name = name
-        self.grades = grades or []
-
-    def take_examp(self, result: int):
-        self.grades.append(result)
-
-
-bob = Student("Bob")
-rolf = Student("Rolf")
-bob.take_examp(19)
-print(bob.grades)
-print(rolf.grades)
+from flask 
