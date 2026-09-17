4ba9a2d3333631ed4eda6dc1d2729ab3e0df73c8
DashPgi <pajiparsa@gmail.com>
2026-09-17 13:44:41 +0330

    Update Advance-Python.MD to include mutability and enhance doc.py with formatting improvements and a decorator implementation for secure access.

--- phase-shish/doc.py
+++ phase-shish/doc.py
@@ -6,24 +6,29 @@ def multiply(*args):
         x *= arg
     return x
 
+
 def plus(*args):
-    x= 1
+    x = 1
     for arg in args:
         x += arg
     return x
+
+
 def minus(*args):
-    x= 1
+    x = 1
     for arg in args:
         x -= arg
     return x
+
+
 def divide(*args):
-    x= 1
+    x = 1
     for arg in args:
         x /= arg
     return x
 
 
-def apply(*args,operator):
+def apply(*args, operator):
     if operator == '+':
         return multiply(*args)
     elif operator == '*':
@@ -35,33 +40,41 @@ def apply(*args,operator):
     else:
         raise ValueError('Invalid operator')
 
+
 operator = str(input('Please enter operator: +, *, /, -: '))
 print(apply(1, 2, 3, operator=operator))
+
 
 # key args
 
 def named(**kwargs):
     print(kwargs)
 
-dict = {"name" : "parsa","age":25}
+
+dict = {"name": "parsa", "age": 25}
 
 named(**dict)
 
 # Type Hinting
 
 from typing import List
-def list_avg(data : List)  -> float:
+
+
+def list_avg(data: List) -> float:
     return sum(list) / len(list)
+
 
 data = 123
 list_avg(data)
+
 
 # Custom error classes
 class TooManyPagesReadError(ValueError):
     pass
 
-class Book :
-    def __init__(self, name : str, page_count: int):
+
+class Book:
+    def __init__(self, name: str, page_count: int):
         self.name = name
         self.page_count = page_count
         self.page_read = 0
@@ -69,7 +82,7 @@ class Book :
     def __repr__(self):
         return f"Book({self.name}, {self.page_count}, {self.page_read})"
 
-    def read(self, pages : int):
+    def read(self, pages: int):
         if self.page_read + pages > self.page_count:
             raise TooManyPagesReadError(
                 f"You tried to read {self.page_read + pages} pages, but you only have {self.page_count} pages."
@@ -77,10 +90,34 @@ class Book :
         self.page_read += pages
         print(self.page_read)
 
+
 python101 = Book("Python", 50)
 
-try :
+try:
     python101.read(66)
     python101.read(60)
 except TooManyPagesReadError as e:
     print(e)
+
+    # Decorators
+
+    users = {"usrname": "aria", "access_lvl": "admin"}
+
+
+    def get_admin_password():
+        return "1234"
+
+
+    def make_secure(func):
+        def secure_function():
+            if users["access_lvl"] == "admin":
+                return func()
+            else:
+                "no admin permissions"
+
+        return secure_function
+
+
+    get_admin_password = make_secure(get_admin_password)
+
+    print(get_admin_password())
