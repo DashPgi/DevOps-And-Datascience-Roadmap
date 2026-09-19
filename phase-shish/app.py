238cb22ff6009b1ea33ea37b63de694dbc6ca7a5
DashPgi <pajiparsa@gmail.com>
2026-09-19 23:04:37 +0330

    Add store data structure and GET endpoint to retrieve stores in app.py

--- phase-shish/app.py
+++ phase-shish/app.py
@@ -1,2 +1,20 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+stores = [
+    {
+        'name': 'pgshop',
+        'items': [
+            {
+                'name': 'chair',
+                'price': 200
+            }
+        ]
+    }
+]
+
+
+@app.get('/store')
+def get_stores():
+    return {"stores": stores}
