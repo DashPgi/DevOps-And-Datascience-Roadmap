da88255868a19228a3a29b8cb1ca59a012e55d49
DashPgi <pajiparsa@gmail.com>
2026-09-21 02:27:07 +0330

    Add POST method to create items in a specific store, enhancing the API's functionality for item management.

--- phase-shish/app.py
+++ phase-shish/app.py
@@ -25,3 +25,12 @@ def create_store():
     new_store = {"name" : request_data["name"],"item" : []}
     stores.append(new_store)
     return new_store, 201
+@app.post("/store/<string:name>/item")
+def creatre_item():
+    request_data = request.get_json()
+    for store in stores :
+        if store["name"] == name
+            new_item = {"name" : request_data["name"], "price" : request_data["price"]}
+            store["items"].append(new_item)
+            return new_item,201
+        return {"message" : "store is not found !"},404
