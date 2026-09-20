ba606dc171e19330f3fedd3e6765d9724a530cf6
DashPgi <pajiparsa@gmail.com>
2026-09-21 02:32:03 +0330

    Add GET methods to retrieve specific stores and items, improving the API's functionality for accessing store data.

--- phase-shish/app.py
+++ phase-shish/app.py
@@ -15,16 +15,20 @@ stores = [
 ]
 
 
+# Create a store and get info
 @app.get('/store')
 def get_stores():
     return {"stores": stores}
 
+# Create items for any shop 
 @app.post("/store")
 def create_store():
     request_data = request.get_json()
     new_store = {"name" : request_data["name"],"item" : []}
     stores.append(new_store)
     return new_store, 201
+
+# Create special item
 @app.post("/store/<string:name>/item")
 def creatre_item():
     request_data = request.get_json()
@@ -34,3 +38,16 @@ def creatre_item():
             store["items"].append(new_item)
             return new_item,201
         return {"message" : "store is not found !"},404
+
+# get a special store or item
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name
+            return store
+        return {"message" : "Store is not found"},404@app.get("/store/<string:name>")
+def get_item_in_store(name):
+    for store in stores:
+        if store["items": store["items"]] == name
+            return store
+        return {"message" : "Store is not found"},404
